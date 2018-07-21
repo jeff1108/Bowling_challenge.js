30c819fce98cb7f4363a01b25902a59d5941e332
jeff1108 <jeffhongkong2007@gmail.com>
2018-07-21 22:49:54 +0100

    check is it spare or strike

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -8,16 +8,31 @@ describe('Frame', function() {
   });
 
   describe('.add', function() {
-    it('add roll score', function() {
+    it('adds roll score', function() {
       expect(frame.add(3)).toEqual([3]);
     });
   });
 
   describe('.score', function() {
-    it('calculate the frame score', function() {
-      frame.add(3)
-      frame.add(3)
+    it('calculates the frame score', function() {
+      frame.add(3);
+      frame.add(3);
       expect(frame.score()).toEqual(6);
+    });
+  });
+
+  describe('.isSpare', function() {
+    it('returns true when it is spare', function() {
+      frame.add(3);
+      frame.add(7);
+      expect(frame.isSpare()).toBeTruthy();
+    });
+  });
+
+  describe('.isStore', function() {
+    it('return true when it is strike', function() {
+      frame.add(10);
+      expect(frame.isStrike()).toBeTruthy();
     });
   });
 });
--- src/Frame.js
+++ src/Frame.js
@@ -10,3 +10,19 @@ Frame.prototype.add = function(scores) {
 Frame.prototype.score = function() {
   return this.frameScore.reduce((total, amount) => total + amount);
 }
+
+Frame.prototype.isSpare = function() {
+  if (this.frameScore.length === 2 && this.score() === 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Frame.prototype.isStrike = function() {
+  if(this.frameScore.length === 1 && this.score() === 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
